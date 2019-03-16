d2801e2de9869e7e923f5bcc61ee3fd13f2f1edb
Aleksandr Kurdiukov <runo@aleksandrs-macbook-pro.local>
2019-03-16 13:56:15 +0100

    Initial Commit

---
+++ App 01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App 01
+//
+//  Created by Aleksandr Kurdiukov on 16.03.19.
+//  Copyright © 2019 Zizlak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
